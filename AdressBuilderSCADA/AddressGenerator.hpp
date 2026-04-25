6c3cbd18dda6a47814e48c1638a58917aed934d3
WildArio <reperti1900@gmail.com>
2026-04-25 18:56:54 +0400

    Remove phantom files

--- AdressBuilderSCADA/AddressGenerator.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "CsvParser.hpp"
-
-// Внимание: generate_address использует глобальное состояние.
-// Не вызывать более одного раза за сессию.
-int generate_address(DataType type);
